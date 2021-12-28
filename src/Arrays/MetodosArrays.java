a78f08e3cf762ac7eba1eeb2b1a3c1595964e583
danksvv <dfvv29@gmail.com>
2021-12-28 10:09:21 +0100

    Ejercicios Clase pruebas

---
+++ src/Arrays/MetodosArrays.java
@@ -0,0 +1,24 @@
+package Arrays;
+import java.util.Scanner;
+
+public class MetodosArrays {
+    public static void main(String[] args) {
+        // Un corredor registra sus vueltas, en este caso 5 vueltas
+        // La máquina informa cuantas vueltas superan el promedio
+        //int[] vueltas = new int[5];
+        int a = 67;
+        System.out.println("valor a : " + a);
+        char b;
+        b = (char)a;
+        System.out.println("valor b : " + b);
+        int num = 4;
+        //++num;
+        num = ++num;
+        System.out.println(num);
+        int z=3;
+        int j=(++z)+(++z)+(--z);
+
+        System.out.println(j);
+
+    }
+}
